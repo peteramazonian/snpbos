ef4d3c7092a2adaa9b6912968df599d003535c22
peteramazonian <64079655+peteramazonian@users.noreply.github.com>
2024-08-25 08:44:33 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the element by its ID
-    var greetingElement = document.getElementById('greeting');
-    
-    // Change the text content of the element
-    greetingElement.textContent = "Hello, GitHub Pages!";
+    console.log("Hello, World!");
 });
